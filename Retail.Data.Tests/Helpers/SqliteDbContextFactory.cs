2e5f9a51e69669f8f7543aa50145ffa4785210d5
Timothy Klenke <timothy.klenke@gmail.com>
2020-05-08 20:37:10 -0600

    Add Sqlite example

---
+++ Retail.Data.Tests/Helpers/SqliteDbContextFactory.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.Common;
+
+namespace Retail.Data.Tests.Helpers
+{
+    public class SqliteDbContextFactory<T> : IDisposable where T : DbContext
+    {
+        private DbConnection _connection;
+
+        public T CreateContext()
+        {
+            if (_connection == null)
+            {
+                var connStrBuilder = new SqliteConnectionStringBuilder { DataSource = ":memory:" };
+
+                _connection = new SqliteConnection(connStrBuilder.ConnectionString);
+                _connection.Open();
+
+                var context = CreateNewContext();
+                context.Database.EnsureCreated();
+                return context;
+            }
+
+            return CreateNewContext();
+        }
+
+        private T CreateNewContext()
+        {
+            var options = CreateOptions();
+            return (T)Activator.CreateInstance(typeof(T), options);
+        }
+
+        private DbContextOptions<RetailContext> CreateOptions()
+        {
+            return new DbContextOptionsBuilder<RetailContext>()
+                .UseSqlite(_connection).Options;
+        }
+
+        public void Dispose()
+        {
+            _connection?.Dispose();
+            _connection = null;
+        }
+    }
+
+}
---
+++ Retail.Data.Tests/SqliteInMemoryDbTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Retail.Data.Models;
+using Retail.Data.Tests.Helpers;
+using System;
+using System.Linq;
+
+namespace Retail.Data.Tests
+{
+    [TestClass]
+    public class SqliteInMemoryDbTests
+    {
+        private static RetailContext _dbRetail;
+        private static SqliteDbContextFactory<RetailContext> _factory;
+
+        [ClassInitialize]
+        public static void ClassSetup(TestContext context)
+        {
+            _factory = new SqliteDbContextFactory<RetailContext>();
+            _dbRetail = _factory.CreateContext();
+        }
+
+        [ClassCleanup]
+        public static void ClassTeardown()
+        {
+            _factory?.Dispose();
+            _dbRetail?.Dispose();
+        }
+
+        [TestMethod]
+        public void SimpleDbTest()
+        {
+            //Add a product
+            var product = new Product()
+            {
+                ProductName = $"Product {Guid.NewGuid()}"
+            };
+            _dbRetail.Add(product);
+            _dbRetail.SaveChanges();
+
+            //Purge all products
+            var products = _dbRetail.Products.ToList();
+            foreach (var p in products)
+                _dbRetail.Remove(p);
+            _dbRetail.SaveChanges();
+        }
+    }
+
+}
