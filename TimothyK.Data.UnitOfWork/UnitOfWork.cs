bd97090ab6b4e34b0327b92c70f43e3cdef23b21
Timothy Klenke <timothy.klenke@gmail.com>
2020-05-14 16:11:30 -0600

    UnitOfWork - base class

---
+++ TimothyK.Data.UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Data.Common;
+
+namespace TimothyK.Data.UnitOfWork
+{
+    public abstract class UnitOfWork : IDisposable
+    {
+        protected DbConnection DbConnection { get; private set; }
+        protected DbTransaction DbTransaction { get; private set; }
+
+        public TContext CreateDbContext<TContext>() where TContext : DbContext
+        {
+            var options = CreateOptions<TContext>();
+            var dbContext = (TContext)Activator.CreateInstance(typeof(TContext), options);
+
+            if (DbConnection != null && dbContext.Database.GetDbConnection() != DbConnection)
+            {
+                throw new InvalidOperationException($"DbContext was created with a different database connection that was previously used.  Check {nameof(CreateOptions)} implementation");
+            }
+            DbConnection = dbContext.Database.GetDbConnection();
+
+            if (DbTransaction == null)
+            {
+                dbContext.Database.BeginTransaction();
+                DbTransaction = dbContext.Database.CurrentTransaction.GetDbTransaction();
+            }
+            else
+            {
+                dbContext.Database.UseTransaction(DbTransaction);
+            }
+
+            return dbContext;
+        }
+
+        protected abstract DbContextOptions<TContext> CreateOptions<TContext>() where TContext : DbContext;
+
+        public void Commit()
+        {
+            DbTransaction.Commit();
+        }
+
+        public void Dispose()
+        {
+            DbTransaction?.Dispose();
+            DbConnection?.Dispose();
+            DbTransaction = null;
+            DbConnection = null;
+        }
+    }
+}
